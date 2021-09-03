865936ec1fc648d5dd6ab9a8fa48a12f3b7901d5
SovietRulez <88634187+SovietRulez@users.noreply.github.com>
2021-09-02 21:17:19 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,16 +4,33 @@ Config.QBTarget = false -- CHANGE IF YOU USE qb-Target
 Config.RobTime = 10
 Config.Shops = {
     taco = {
-            name = 'Taco Party',
-            location = vector3(373.77, 326.09, 103.57),
-            bossLocation = vector3(376.54, 325.46, 103.57),
-            price = 1,
-            allowedItems = { 'sandwich', 'lockpick'},
-            robLocation = vector3(373.1, 329.05, 103.57),
-            robbed = false,
-            cooldown = 10,
-            onC = false
-    },
+        name = 'Taco Party',
+        location = vector3(373.77, 326.09, 103.57),
+        bossLocation = vector3(376.54, 325.46, 103.57),
+        price = 1,
+        allowedItems = {
+            [1] = {
+                name = 'sandwich',
+                price = 0,
+                amount = 10,
+               -- type = 'item',
+               -- info = {},
+                slot = 1
+            }, 
+            [2] = {
+                name = 'lockpick',
+                price = 0,
+                amount = 10,
+               -- type = 'item',
+               -- info = {},
+                slot = 2
+            }
+        },
+        robLocation = vector3(373.1, 329.05, 103.57),
+        robbed = false,
+        cooldown = 10,
+        onC = false
+},
     -- [2] = {
     --     name = 'test2',
     --     location = vector3(0, 0, 0),
