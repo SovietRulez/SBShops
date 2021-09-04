d95cbad6b43e6376016292bb1544b19c74c632f9
SovietRulez <88634187+SovietRulez@users.noreply.github.com>
2021-09-04 22:45:43 +0000

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,24 +1,17 @@
-QBCore.Functions.CreateCallback('sellShop', function(source, cb, target, shopName)
+QBCore.Functions.CreateCallback('sellShop', function(source, cb, target, globalVar)
     local src = source
     local targetPlayer = QBCore.Functions.GetPlayer(target).PlayerData
     local targetCID = targetPlayer.citizenid
+    local shopInfo = Config.Shops[globalVar]
     local result = exports.ghmattimysql:executeSync('SELECT * FROM sbshops WHERE citizenid=@citizenid', {['@citizenid'] = targetCID})
-    print(target)
-    print(json.encode(result[1]))
-    print(shopName)
-    print(targetCID)
     if not result[1] then
         TriggerClientEvent("QBCore:Notify", src, "Shop has been sold", "success", 5000)
         exports['ghmattimysql']:execute('INSERT INTO sbshops (citizenid, shopName) VALUES (@citizenid, @shopName)', {
             ['citizenid'] = targetCID,
-            ['shopName'] = shopName
+            ['shopName'] = globalVar
         })
     elseif result[1] then
         TriggerClientEvent("QBCore:Notify", src, "Shop is already owned!", "error", 5000)
     end
 end)
-
 ------------SELL SHOP----------------^^^^^^^^^^^^^^^^^^^
-RegisterCommand('testprint', function(source)
-    print(json.encode(Config.Shops))
-end, false)
