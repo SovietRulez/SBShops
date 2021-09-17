f5d8943b3ffa06a9d245c041300ebb9d76249356
SovietRulez <88634187+SovietRulez@users.noreply.github.com>
2021-09-16 21:34:40 -0700

    Pushed oxmysql update

--- server/main.lua
+++ server/main.lua
@@ -6,8 +6,8 @@ QBCore.Functions.CreateCallback('sellShop', function(source, cb, target, shopInf
     local Player2 = QBCore.Functions.GetPlayer(src)
     local targetPlayer = QBCore.Functions.GetPlayer(target).PlayerData
     local targetCID = targetPlayer.citizenid
-    local result = exports.ghmattimysql:executeSync('SELECT * FROM sbshops WHERE shopName=@shopName', {
-        ['@shopName'] = shopInfo.name
+    local result = exports['oxmysql']:executeSync('SELECT * FROM sbshops WHERE shopName=:shopName', {
+        ['shopName'] = shopInfo.name
     })
     if result[1] and Player.PlayerData.money.bank >= shopInfo.price then
         if not result[1].citizenid then
@@ -20,9 +20,9 @@ QBCore.Functions.CreateCallback('sellShop', function(source, cb, target, shopInf
             TriggerClientEvent("QBCore:Notify", src, string.format(
                 shopInfo.name .. " has been sold to %s %s and money deposited with %s", targetFn, targetLn, Config.Job), "success", 5000)
             TriggerClientEvent("QBCore:Notify", target, "You now own the shop " .. shopInfo.name, "success", 5000)
-            exports['ghmattimysql']:execute('UPDATE sbshops SET citizenid = @citizenid WHERE shopName = @shopName', {
-                ['@citizenid'] = targetCID,
-                ['@shopName'] = shopInfo.name
+            exports['oxmysql']:execute('UPDATE sbshops SET citizenid = :citizenid WHERE shopName = :shopName', {
+                ['citizenid'] = targetCID,
+                ['shopName'] = shopInfo.name
             })
         else
             local player = QBCore.Functions.GetPlayerByCitizenId(result[1].citizenid)
@@ -37,13 +37,13 @@ end)
 
 QBCore.Functions.CreateCallback('repoShop', function(source, cb, globalVar, shopInfo)
     local src = source
-    local result = exports.ghmattimysql:executeSync('SELECT * FROM sbshops WHERE shopName=@shopName', {
-        ['@shopName'] = globalVar
+    local result = exports['oxmysql']:executeSync('SELECT * FROM sbshops WHERE shopName=:shopName', {
+        ['shopName'] = globalVar
     })
     if result[1] then
         if result[1].citizenid then
-            exports.ghmattimysql:execute('UPDATE sbshops SET citizenid = NULL WHERE shopName=@shopName', {
-                ['@shopName'] = globalVar
+            exports['oxmysql']:execute('UPDATE sbshops SET citizenid = NULL WHERE shopName=:shopName', {
+                ['shopName'] = globalVar
             }, function()
                 TriggerClientEvent("QBCore:Notify", src, string.format("%s has been reposessed", globalVar), "success", 5000)
             end)
@@ -53,15 +53,14 @@ QBCore.Functions.CreateCallback('repoShop', function(source, cb, globalVar, shop
     end
 end)
 
-QBCore.Functions.CreateCallback('isOwner', function(source, cb, shopname)
+QBCore.Functions.CreateCallback('isOwner', function(source, cb, shopName)
     local src = source
     local cid = QBCore.Functions.GetPlayer(src).PlayerData.citizenid
-    local result = exports.ghmattimysql:executeSync(
-        'SELECT * FROM sbshops WHERE shopName=@shopName AND citizenid = @citizenid', {
-            ['@shopName'] = shopname,
-            ['@citizenid'] = cid
-        })
-    if result[1] then
+    print(shopName, cid)
+    local result = exports.oxmysql:scalarSync('SELECT 1 FROM sbshops WHERE shopname = ? AND citizenid = ?', {shopName, cid})
+    print(result)
+
+    if result then
         cb(true)
     else
         cb(false)
@@ -71,10 +70,10 @@ end)
 QBCore.Functions.CreateCallback('accountAmount', function(source, cb, shopInfo, amt)
     local src = source
     local cid = QBCore.Functions.GetPlayer(src).PlayerData.citizenid
-    local result = exports.ghmattimysql:executeSync(
-        'SELECT * FROM sbshops WHERE shopName=@shopName AND citizenid = @citizenid', {
-            ['@shopName'] = shopInfo.name,
-            ['@citizenid'] = cid
+    local result = exports['oxmysql']:executeSync(
+        'SELECT * FROM sbshops WHERE shopName=:shopName AND citizenid = :citizenid', {
+            ['shopName'] = shopInfo.name,
+            ['citizenid'] = cid
         })
     local amt = result[1].accountMoney
     if result[1].accountMoney then
@@ -92,18 +91,18 @@ AddEventHandler('withdraw', function(withdrawAmount, shopInfo)
     local spotLoc = #(shopInfo.locations.boss - plyLoc)
     local Player = QBCore.Functions.GetPlayer(src)
     local cid = QBCore.Functions.GetPlayer(src).PlayerData.citizenid
-    local result = exports.ghmattimysql:executeSync(
-        'SELECT * FROM sbshops WHERE shopName=@shopName AND citizenid = @citizenid', {
-            ['@shopName'] = shopInfo.name,
-            ['@citizenid'] = cid,
-            ['@accountMoney'] = 0
+    local result = exports['oxmysql']:executeSync(
+        'SELECT * FROM sbshops WHERE shopName=:shopName AND citizenid = :citizenid', {
+            ['shopName'] = shopInfo.name,
+            ['citizenid'] = cid,
+            ['accountMoney'] = 0
         })
     if spotLoc < 2 then
         if result[1].accountMoney >= withdrawAmount then
-            exports.ghmattimysql:execute(
-                'UPDATE sbshops SET accountMoney = accountMoney - @withdrawAmount WHERE shopName=@shopName', {
-                    ['@shopName'] = shopInfo.name,
-                    ['@withdrawAmount'] = withdrawAmount
+            exports['oxmysql']:execute(
+                'UPDATE sbshops SET accountMoney = accountMoney - :withdrawAmount WHERE shopName=:shopName', {
+                    ['shopName'] = shopInfo.name,
+                    ['withdrawAmount'] = withdrawAmount
                 }, function()
                     TriggerClientEvent("QBCore:Notify", src, string.format("%s has withdrawn %s", shopInfo.name, withdrawAmount), "success", 5000)
                     Player.Functions.AddMoney('bank', withdrawAmount)
@@ -121,16 +120,16 @@ RegisterNetEvent('robberyAmount', function(globalVar)
     local spotLoc = #(globalVar.locations.robLocation - plyLoc)
     local Player = QBCore.Functions.GetPlayer(src)
     local cid = QBCore.Functions.GetPlayer(src).PlayerData.citizenid
-    local result = exports.ghmattimysql:executeSync(
-        'SELECT * FROM sbshops WHERE shopName=@shopName AND citizenid = @citizenid', {
-            ['@shopName'] = globalVar.name,
-            ['@citizenid'] = cid,
-            ['@accountMoney'] = 0
+    local result = exports['oxmysql']:executeSync(
+        'SELECT * FROM sbshops WHERE shopName=:shopName AND citizenid = :citizenid', {
+            ['shopName'] = globalVar.name,
+            ['citizenid'] = cid,
+            ['accountMoney'] = 0
         })
     if spotLoc < 2 then
-        exports.ghmattimysql:execute('UPDATE sbshops SET accountMoney = @acctMny WHERE shopName=@shopName', {
-            ['@shopName'] = globalVar.name,
-            ['@acctMny'] = result[1].accountMoney - result[1].accountMoney / Config.Percent
+        exports['oxmysql']:execute('UPDATE sbshops SET accountMoney = :acctMny WHERE shopName=:shopName', {
+            ['shopName'] = globalVar.name,
+            ['acctMny'] = result[1].accountMoney - result[1].accountMoney / Config.Percent
         }, function()
             local val = result[1].accountMoney / Config.Percent
 
@@ -150,17 +149,17 @@ AddEventHandler('deposit', function(depositAmount, shopInfo)
     local Player = QBCore.Functions.GetPlayer(src)
     local cid = Player.PlayerData.citizenid
     local money = Player.PlayerData.money['cash']
-    local result = exports.ghmattimysql:executeSync(
-        'SELECT * FROM sbshops WHERE shopName=@shopName AND citizenid = @citizenid', {
-            ['@shopName'] = shopInfo.name,
-            ['@citizenid'] = cid,
-            ['@accountMoney'] = 0
+    local result = exports['oxmysql']:executeSync(
+        'SELECT * FROM sbshops WHERE shopName=:shopName AND citizenid = :citizenid', {
+            ['shopName'] = shopInfo.name,
+            ['citizenid'] = cid,
+            ['accountMoney'] = 0
         })
     if spotLoc < 2 then
         if result[1].accountMoney and money >= depositAmount then
-            exports.ghmattimysql:execute('UPDATE sbshops SET accountMoney = accountMoney + @depositAmount WHERE shopName=@shopName', {
-                    ['@shopName'] = shopInfo.name,
-                    ['@depositAmount'] = depositAmount
+            exports['oxmysql']:execute('UPDATE sbshops SET accountMoney = accountMoney + :depositAmount WHERE shopName=:shopName', {
+                    ['shopName'] = shopInfo.name,
+                    ['depositAmount'] = depositAmount
                 }, function()
                     TriggerClientEvent("QBCore:Notify", src, string.format("You have deposited %s in %s store",depositAmount, shopInfo.name), "success", 5000)
                     Player.Functions.RemoveMoney('cash', depositAmount)
@@ -182,7 +181,7 @@ AddEventHandler('test', function(itemQuantity, shopName, itemName, itemPrice, sl
     local cid = Player.PlayerData.citizenid
     local money = Player.PlayerData.money['cash']
     local moneyAmount = itemPrice * itemQuantity
-    local items = json.decode(exports.ghmattimysql:scalarSync('SELECT items FROM sbshops WHERE shopName=@shopName AND citizenid = @citizenid', { ['@shopName'] = shopName.name, ['@citizenid'] = cid }))
+    local items = json.decode(exports['oxmysql']:scalarSync('SELECT items FROM sbshops WHERE shopName=:shopName AND citizenid = :citizenid', { ['shopName'] = shopName.name, ['citizenid'] = cid }))
     if not items then items = {} end
     if #items == 0 then
         table.insert(items, {
@@ -210,9 +209,9 @@ AddEventHandler('test', function(itemQuantity, shopName, itemName, itemPrice, sl
         end
     end
 
-    exports.ghmattimysql:execute('UPDATE sbshops SET items = @items WHERE shopName=@shopName', {
-        ['@shopName'] = shopName.name,
-        ['@items'] = json.encode(items)
+    exports['oxmysql']:execute('UPDATE sbshops SET items = :items WHERE shopName=:shopName', {
+        ['shopName'] = shopName.name,
+        ['items'] = json.encode(items)
     }, function()
         TriggerClientEvent("QBCore:Notify", src, string.format("You have bought %s %s worth $%s for store %s", itemQuantity, itemName, moneyAmount, shopName.name), "success", 5000)
         Player.Functions.RemoveMoney('cash', moneyAmount)
@@ -220,15 +219,15 @@ AddEventHandler('test', function(itemQuantity, shopName, itemName, itemPrice, sl
 end)
 
 QBCore.Functions.CreateCallback('SBShops:GetShopInvData', function(source, cb, shopName)
-    local items = exports.ghmattimysql:scalarSync('SELECT items FROM sbshops WHERE shopName=@shopName', { ['@shopName'] = shopName })
+    local items = exports['oxmysql']:scalarSync('SELECT items FROM sbshops WHERE shopName=@shopName', { ['@shopName'] = shopName })
     cb(json.decode(items))
 end)
 
 RegisterServerEvent('qb-shops:server:UpdateShopItems')
 AddEventHandler('qb-shops:server:UpdateShopItems', function(shop, data, amount)
-  local result = exports.ghmattimysql:executeSync(
-  'SELECT * FROM sbshops WHERE shopName=@shopName', {
-      ['@shopName'] = Config.Shops[shop].name
+  local result = exports['oxmysql']:executeSync(
+  'SELECT * FROM sbshops WHERE shopName=:shopName', {
+      ['shopName'] = Config.Shops[shop].name
   })
   local items = json.decode(result[1].items)
   local accMon = result[1].accountMoney
@@ -243,16 +242,16 @@ AddEventHandler('qb-shops:server:UpdateShopItems', function(shop, data, amount)
       end
     end
   end
-  exports.ghmattimysql:execute('UPDATE sbshops SET items = @items, accountMoney = @acMon WHERE shopName=@shopName', {
-      ['@shopName'] = Config.Shops[shop].name,
-      ['@acMon'] = accMon+data.price*amount,
-      ['@items'] = json.encode(items)
+  exports['oxmysql']:execute('UPDATE sbshops SET items = :items, accountMoney = :acMon WHERE shopName=:shopName', {
+      ['shopName'] = Config.Shops[shop].name,
+      ['acMon'] = accMon+data.price*amount,
+      ['items'] = json.encode(items)
   })
 end)
 
 Citizen.CreateThread(function()
   Wait(5000)
-  local shops = exports.ghmattimysql:executeSync('SELECT * FROM sbshops ', {})
+  local shops = exports['oxmysql']:executeSync('SELECT * FROM sbshops ', {})
   for i = 1,#shops do
     if shops[i].citizenid == nil or shops[i].citizenid == '' then
       local items = {}
@@ -263,9 +262,9 @@ Citizen.CreateThread(function()
           end
         end
       end
-      exports.ghmattimysql:execute('UPDATE sbshops SET items = @items WHERE shopName=@shopName', {
-          ['@shopName'] = shops[i].shopName,
-          ['@items'] = json.encode(items)
+      exports['oxmysql']:execute('UPDATE sbshops SET items = :items WHERE shopName=:shopName', {
+          ['shopName'] = shops[i].shopName,
+          ['items'] = json.encode(items)
       })
     end
   end
@@ -285,18 +284,12 @@ QBCore.Functions.CreateCallback('soviet:server:getCops', function(source, cb)
 end)
 
 
-RegisterServerEvent('soviet:server:syncStatus')
-AddEventHandler('soviet:server:syncStatus', function(shopData, shopName, robbed,  cooling)
-    if robbed ~= nil then
-        Config.Shops[shopName][shopData].robbed = robbed
+RegisterServerEvent('soviet:server:startCooldown')
+AddEventHandler('soviet:server:startCooldown', function(globalVar, shopData)
+    print(Config.Shops[globalVar].name.onC)
+    if not Config.Shops[globalVar].onC then
+        Config.Shops[globalVar].onC = true
+        Wait(Config.Shops[globalVar].cooldown * 1000)
+        Config.Shops[globalVar].onC = false
     end
-    if cooling ~= nil then
-        Config.Shops[shopName][shopData].onC = cooling
-    end
-    TriggerClientEvent('soviet:client:syncStatus', -1, shopData, shopName,robbed, cooling)
-end)
-
-RegisterServerEvent('soviet:server:getZones')
-AddEventHandler('soviet:server:getZones', function()
-    TriggerClientEvent('soviet:client:syncZones', source, Config.Shops)
 end)
