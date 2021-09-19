40e81ffedbf9649c405a7060f833e89574ef4a84
SovietRulez <88634187+SovietRulez@users.noreply.github.com>
2021-09-18 23:07:32 -0700

    Blackadata

--- config.lua
+++ config.lua
@@ -1,20 +1,20 @@
-Config = {}
+Config = {} --DONT TOUCH, IDK WHY I MUST SAY THIS!
 Config.Job = 'realestate' -- JOB FOR SALE/REPO
 Config.QBTarget = false -- NOT USED CURRENTLY, PLANNED
 Config.RobTime = 360-- TIME FOR ROBBERY
 Config.Percent = 10 -- PERCENT TO ROB
-Config.CopsRequired = 0
+Config.CopsRequired = 0 -- COPS REQUIRED
+Config.PercentToSell = 75 -- EXAMPLES 50 means 50% off, 75 means 25% off
 Config.Shops = {
     taco = {
-        name = 'Taco Party',
+        name = 'Shop1', -- Make sure this name matches the name within the database!
         locations = {
             boss = vector3(379.66, 332.57, 103.57),
-           
             customer = vector3(373.77, 326.09, 103.57),
             robLocation = vector3(373.1, 329.05, 103.57),
             realEstate = vector3(378.57, 321.35, 103.39)
         },
-        price = 1,
+        price = 1, -- Price for shop sale
         allowedItems = {
             [1] = {
                 name = 'sandwich',
@@ -33,14 +33,20 @@ Config.Shops = {
                 price = 10,
                 amount = 100,
                 slot = 3
-            }
+            },
+            [4] = {
+                name = 'repairkit',
+                price = 10,
+                amount = 1000,
+                slot = 3
+            },
         },
-        robbed = false,
+        robbed = false, -- DONT TOUCH
         cooldown = 1000, --ROBBERY COOLDOWN
-        onC = false
+        onC = false -- DONT TOUCH
     },
     nacho = {
-        name = 'SovietRulez',
+        name = 'Shop2',
         locations = {
             boss = vector3(27.32, -1343.27, 29.5),
             customer = vector3(25.65, -1346.05, 29.5),
@@ -73,7 +79,7 @@ Config.Shops = {
         onC = false
     },
     savage = {
-        name = 'T Party',
+        name = 'Shop3',
         locations = {
             boss = vector3(-709.57, -905.75, 19.22),
             customer = vector3(-707.42, -912.79, 19.22),
@@ -100,7 +106,7 @@ Config.Shops = {
         onC = false
     },
     lips = {
-        name = 'Lip Party',
+        name = 'Shop4',
         locations = {
             boss = vector3(-1479.43, -372.66, 39.16),
             customer = vector3(-1487.05, -379.34, 40.16),
@@ -127,7 +133,7 @@ Config.Shops = {
         onC = false
     },
     llcooljay = {
-        name = 'LL Party',
+        name = 'Shop5',
         locations = {
             boss = vector3(1160.01, -315.98, 69.21),
             customer = vector3(1163.57, -323.95, 69.21),
@@ -154,7 +160,7 @@ Config.Shops = {
         onC = false
     },
     testr = {
-        name = 'Testing Party',
+        name = 'Shop6',
         locations = {
             boss = vector3(1735.79, 6419.77, 35.04),
             customer = vector3(1728.6, 6414.07, 35.04),
@@ -174,14 +180,20 @@ Config.Shops = {
                 price = 10,
                 amount = 100,
                 slot = 2
-            }
+            }--[[,
+            [3] = {
+                name = 'repairkit',
+                price = 10,
+                amount = 1000,
+                slot = 3
+            }]]
         },
         robbed = false,
         cooldown = 10,
         onC = false
     },
     eat = {
-        name = 'Eat Party',
+        name = 'Shop7',
         locations = {
             boss = vector3(1706.38, 4920.83, 42.08),
             customer = vector3(1698.08, 4924.41, 42.06),
@@ -208,7 +220,7 @@ Config.Shops = {
         onC = false
     },
     xrated = {
-        name = 'Xrated Party',
+        name = 'Shop8',
         locations = {
             boss = vector3(2674.17, 3287.4, 55.24),
             customer = vector3(2679.31, 3280.12, 55.24),
@@ -235,7 +247,7 @@ Config.Shops = {
         onC = false
     },
     sorry = {
-        name = 'Sorry Party',
+        name = 'Shop9',
         locations = {
             boss = vector3(1960.45, 3749.04, 32.34),
             customer = vector3(1961.51, 3740.1, 32.34),
@@ -262,7 +274,7 @@ Config.Shops = {
         onC = false
     },
     stupid = {
-        name = 'Stupid Party',
+        name = 'Shop10',
         locations = {
             boss = vector3(544.32, 2662.76, 42.21),
             customer = vector3(547.64, 2671.73, 42.16),
@@ -289,7 +301,7 @@ Config.Shops = {
         onC = false
     },
     testing = {
-        name = 'Test Party',
+        name = 'Shop11',
         locations = {
             boss = vector3(-3249.29, 1005.8, 12.83),
             customer = vector3(-3241.47, 1001.14, 12.83),
@@ -316,7 +328,7 @@ Config.Shops = {
         onC = false
     },
     hewasnt = {
-        name = 'Fasho Party',
+        name = 'Shop12',
         locations = {
             boss = vector3(-3047.76, 586.82, 7.91),
             customer = vector3(-3038.78, 585.97, 7.91),
@@ -343,7 +355,7 @@ Config.Shops = {
         onC = false
     },
     james = {
-        name = 'James Party',
+        name = 'Shop13',
         locations = {
             boss = vector3(-2958.28, 389.07, 14.04),
             customer = vector3(-2967.79, 391.67, 15.04),
@@ -370,7 +382,7 @@ Config.Shops = {
         onC = false
     },
     wheels = {
-        name = 'Wheels Party',
+        name = 'Shop14',
         locations = {
             boss = vector3(2549.63, 385.79, 108.62),
             customer = vector3(2557.98, 382.05, 108.62),
@@ -397,7 +409,7 @@ Config.Shops = {
         onC = false
     },
     loose = {
-        name = 'Tea Party',
+        name = 'Shop15',
         locations = {
             boss = vector3(-1218.56, -916.18, 11.33),
             customer = vector3(-1222.83, -907.18, 12.33),
@@ -424,7 +436,7 @@ Config.Shops = {
         onC = false
     },
     rico = {
-        name = 'Rico Party',
+        name = 'Shop16',
         locations = {
             boss = vector3(1125.72, -983.01, 45.42),
             customer = vector3(1135.7, -982.77, 46.42),
@@ -451,7 +463,7 @@ Config.Shops = {
         onC = false
     },
     burrito = {
-        name = 'Burrito Party',
+        name = 'Shop17',
         locations = {
             boss = vector3(-44.17, -1749.51, 29.42),
             customer = vector3(-48.09, -1757.62, 29.42),
